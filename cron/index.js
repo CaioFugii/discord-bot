2e8f3e2a8ff9e7bbaff0b01e0a535ed6614b11cb
caiofugii <dev@fastprobr.com>
2021-01-13 22:07:29 -0300

    changes

---
+++ cron/index.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const cron = require('node-cron');
+
+const createInstance = (url) => {
+    let headers = {
+      "X-Riot-Token": api_league,
+    };
+    
+    return axios.create({
+      baseURL: url,
+      headers,
+    });
+  };
+
+const instance = (url) => createInstance(url);
+
+const cronRoutine = () => {
+    cron.schedule('*/25 9-23 * * *', async () => {
+        return await instance('https://brp-discordbot.herokuapp.com').get(`/`)
+    });
+}
+
+module.exports = { cronRoutine }
--- express.js
+++ express.js
@@ -6,14 +6,21 @@ const http = require('http')
 const app = express();
 
 const { startBot } = require('./bot')
+const { cronRoutine } = require('./cron')
 
 const server = http.createServer(app)
 
 const port = PORT || '8080';
 
+const router = express.Router();
+
+router.get('/',(req, res) => {
+    return res.status(200).json({ message: 'Avoid idling' })
+});
 
 server.listen(port, () => {
     startBot()
+    cronRoutine()
     console.log(`BOT running on port ${port}.`);
 });
 
